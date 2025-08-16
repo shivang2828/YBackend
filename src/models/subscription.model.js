dd81527283131f74883c06af20eae84105748919
Shivang Gaur <shivangbabu28.sgg@gmail.com>
2025-08-16 14:37:20 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import e from "express";
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+  subscriber: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+
+channel :{
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+}
+
+});
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
